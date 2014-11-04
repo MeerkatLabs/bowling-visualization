e2e2ea04901ca320c33792245fdb80c33f04ec6b
Robert Robinson <rerobins@gmail.com>
2014-11-03 17:24:35 -0700

    Add getData overrides for multiple views.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,22 @@ var phonecatControllers = angular.module('leaguecontrollers', []);
 phonecatControllers.factory("dataProvider", ['$q', function($q) {
         console.log("Running dataProvider factory");
 
+        var dataLoaded = false;
+
         return {
             getData: function() {
-                var result = bowling.initialize({"root": "testdata"}, $q);
-                return result;
+                if (!dataLoaded) {
+                    var result = bowling.initialize({"root": "testdata"}, $q);
+                    return result.then(function (league) {
+                        console.log("Data loaded marking flag");
+                        dataLoaded = true;
+                        return league;
+                    });
+                } else {
+                    return $q(function (resolve, reject) {
+                       resolve(bowling.currentLeague);
+                    });
+                }
             }
         }
     }]).controller('LeagueController', ['$scope', '$route', 'dataProvider',
