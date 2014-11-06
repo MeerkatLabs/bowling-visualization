7862685f63debeb9b978c9bda867b6133e4ef890
Robert Robinson <rerobins@gmail.com>
2014-11-05 20:09:12 -0700

    Add support for D3
    Update CumulativeScoreSheet and MatchDetail to not use arrays in order to access the data.
    
    Add D3 library to bower.
    Add simple implementation to the match detail in order to show how to use d3.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -44,6 +44,47 @@ phonecatControllers.factory("dataProvider", ['$q', function($q) {
                 });
                 console.log(foundMatch);
                 $scope.match = foundMatch;
+
+                var circleData = [
+                    { "cx": 20, "cy": 20, "radius": 20, "color" : "green" },
+                    { "cx": 70, "cy": 70, "radius": 20, "color" : "purple" }];
+
+                $scope.circles = circleData;
             });
-}]);
+    }]).directive('bargraph', function() {
+        function link(scope, element, attrs) {
+
+            scope.render = function() {
+                if (!scope.circles) { return; }
+
+                var svg = d3.select(element[0])
+                    .append("svg")
+                    .style('width', '100%');
+
+                //Add circles to the svgContainer
+                var circles = svg.selectAll("circle")
+                                           .data(scope.circles)
+                                           .enter()
+                                           .append("circle");
+
+                //Add the circle attributes
+                var circleAttributes = circles
+                                       .attr("cx", function (d) { return d.cx; })
+                                       .attr("cy", function (d) { return d.cy; })
+                                       .attr("r", function (d) { return d.radius; })
+                                       .style("fill", function (d) { return d.color; });
+            };
+
+            scope.$watch('circles', function(newVals, oldVals) {
+                return scope.render();
+            }, true);
+        }
+
+        return {
+            link: link,
+            scope: {
+                circles: '=data'
+            }
+        }
+    });
 
