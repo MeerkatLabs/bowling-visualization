f2c3f91420f7ee6677342b0b29f94f21131e356c
Robert Robinson <rerobins@gmail.com>
2014-11-09 11:57:58 -0700

    Clean up Week list page.

--- app/weeks/WeekController.js
+++ app/weeks/WeekController.js
@@ -7,8 +7,19 @@ var bowlingApp = bowlingApp || angular.module('bowling');
 bowlingApp.controller('LeagueController', ['$scope', 'dataProvider',
         function ($scope, dataProvider) {
 
+            var splitSize = 3;
+
             dataProvider.getData().then(function (league) {
                 $scope.league = league;
+
+                var leagueWeeks = league.weeks.concat();
+                var split = [];
+                while (leagueWeeks.length > 0) {
+                    split.push(leagueWeeks.splice(0,splitSize));
+                }
+
+                $scope.splits = split;
+
             });
 
         }
