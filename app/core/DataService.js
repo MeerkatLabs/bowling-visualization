f9b39f61329490911c6d215051c09c179cc2d50a
Robert Robinson <rerobins@gmail.com>
2014-11-20 16:18:34 -0700

    Removing reliance on jquery loading data
    Instead of using jquery to load the data, now using the angular $http service.  This should improve the testing.
    
    Fixes #28

--- app/core/DataService.js
+++ app/core/DataService.js
@@ -13,20 +13,19 @@
      * @param {Object} configuration
      * @param {string} configuration.root the location of the data files.
      * @param {Object} $q
+     * @param {Object} $http
      * @constructor
      */
-    function DataService(configuration, $q) {
-        var promise = null;
+    function DataService(configuration, $q, $http) {
+        var deferred = null;
         var myConfiguration = configuration;
-
-        promise = bowling.initialize(myConfiguration, $q);
 
         /**
          * Load the league data and results.
          * @returns {*} promise
          */
         this.getData = function() {
-            return promise;
+            return deferred.promise;
         };
 
         /**
@@ -36,6 +35,89 @@
         this.getConfiguration = function() {
             return myConfiguration;
         };
+
+        /**
+         * Load the league data from the configuration details.
+         */
+        this.load = function () {
+
+            deferred = $q.defer();
+
+            var service = this;
+
+            var root = this.getConfiguration().root;
+            $http.get(root + '/league.json').success(function (data) {
+                service._handleLeagueLoad(data);
+            }).error(function (error) {
+                if (error.status == 200) {
+                    console.error("File found, but not parsable.");
+                } else {
+                    console.error("Couldn't find week data for: " + weekNumber);
+                }
+            });
+        };
+
+        /**
+         * Method that will handle the data that is loaded from the league file.
+         * @param {Object} data
+         * @param {Array} data.teams
+         * @param {int} data.weeks
+         * @private
+         */
+        this._handleLeagueLoad = function (data) {
+            console.log('Loaded league data for: ' + data.name);
+            bowling.currentLeague = new bowling.League(data);
+
+            if (bowling.currentLeague.weekCount > 0) {
+                this.loadWeek(1);
+            } else {
+                console.warn("League doesn't contain any week data");
+            }
+        };
+
+        /**
+         * Load the week data for the league.
+         * @param {int} weekNumber
+         */
+        this.loadWeek = function (weekNumber) {
+            console.log("Attempting to load week: " + weekNumber);
+            var service = this;
+            $http.get(this.getConfiguration().root + '/week' + weekNumber + '.json').success(function (data) {
+                service._handleWeekLoad(weekNumber, data);
+            }).error(function (error) {
+                if (error.status == 200) {
+                    console.log("File found, but not parsable.");
+                } else {
+                    console.log("Couldn't find week data for: " + weekNumber);
+                }
+                console.log(error);
+                deferred.resolve(bowling.currentLeague);
+            });
+        };
+
+        /**
+         * Handle the loading of the week data.
+         * @param {int} weekNumber
+         * @param {Object} data
+         * @param {Array} data.scoresheet
+         * @param {string} data.date
+         * @private
+         */
+        this._handleWeekLoad = function (weekNumber, data) {
+            // Need to create a unique series for each of the objects that defined, then associate all of the data with
+            // the appropriate team and/or player.
+            var week = new bowling.Week(data, weekNumber);
+            bowling.currentLeague.weeks.push(week);
+
+            if (weekNumber + 1 <= bowling.currentLeague.weekCount) {
+                this.loadWeek(weekNumber + 1);
+            } else {
+                deferred.resolve(bowling.currentLeague);
+            }
+
+        };
+
+        this.load();
 
     }
 
@@ -56,8 +138,8 @@
         };
 
         // This actually creates the data service with the promise API and the configuration object.
-        this.$get = ['$q', function ($q) {
-            return new DataService(configuration, $q);
+        this.$get = ['$q', '$http', function ($q, $http) {
+            return new DataService(configuration, $q, $http);
         }];
     }
 
--- app/model/bowling.js
+++ app/model/bowling.js
@@ -23,30 +23,6 @@
          */
         currentLeague: null,
 
-        /**
-         * Default configuration object for the library.
-         */
-        configuration: {
-            root: 'leaguedata'
-        },
-
-        /**
-         * Initialize the bowling data model.
-         * @param {Object} configuration
-         * @param $q Angular JS promise implementation
-         * @returns angular promise.
-         */
-        initialize: function (configuration, $q) {
-
-            bowling.configuration = configuration || bowling.configuration;
-
-            var deferred = $q.defer();
-            var leagueLoader = new bowling.LeagueLoader(deferred);
-            leagueLoader.load();
-
-            return deferred.promise;
-        }
-
     };
 
     // Install the bowling namespace into the global scope.
